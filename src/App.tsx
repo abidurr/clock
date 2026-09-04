9bbe05936909219db0a0c9070fd6d857611a45bc
Abidur Rahim <42abid@gmail.com>
2026-09-04 11:43:16 -0400

    test gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,32 @@ import logo from "./logo.svg"
 import "./App.css"
 
 function App() {
+  const [time, setTime] = React.useState(new Date())
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date())
+    }, 1000)
+    return () => clearInterval(timer)
+  }, [])
+
   return (
-    <div className="App">
+    <div
+      className="App"
+      // onClick={() => {
+      //   document.querySelector(".toggle").addEventListener("click", (event) => {
+      //     if (document.fullscreenElement) {
+      //       // If there is a fullscreen element, exit full screen.
+      //       document.exitFullscreen()
+      //       return
+      //     }
+      //     // Make the .element div fullscreen.
+      //     document.querySelector(".element").requestFullscreen()
+      //   })
+      // }}
+    >
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/*<img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -17,7 +39,10 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a>*/}
+        <p>Clock</p>
+        <p>{time.toLocaleTimeString()}</p>
+        <p>{time.toLocaleDateString()}</p>
       </header>
     </div>
   )
